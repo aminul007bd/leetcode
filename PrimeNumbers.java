4a37b749297cf06a61c1adf7405cf1b0eb72d3f2
aminul007bd <gem007bd@gmail.com>
2022-02-14 12:17:43 -0500

    prime number count solution

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -3,6 +3,39 @@ import java.util.List;
 
 public class PrimeNumbers {
 
+    public static int primeNumbers(int n) {
+        int primeCount = 0;
+        boolean[] primes = new boolean[n];
+
+        // i * i because it should not more than the square
+        for(int i = 2; i * i < primes.length; i ++) {
+            // if it is not 2,4,6,8 or 3, 9, 12...
+            if(primes[i] == false) {
+                for(int j = i; j * i < primes.length; j++) {
+                    primes [i * j] = true;
+                }
+            }
+        }
+
+        // Leetcode approach
+        for(int i = 0; i <= (int)Math.sqrt(n); i++) {
+            if(primes[i] == false){
+                for(int j = i * i; j < n; j+= i) {
+                    primes[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if(primes[i] == false) {
+                primeCount++;
+            }
+        }
+
+        return primeCount;
+
+    }
+
         public static void main (String args []) {
             // A prime number is a whole number greater than 1
             // that has only two factors and that is 1 and the number ifself
