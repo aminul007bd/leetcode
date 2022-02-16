f9518283ac17a7ef19c3738fdcf0e9689b0c2835
aminul007bd <gem007bd@gmail.com>
2022-02-16 11:34:20 -0500

    single number

---
+++ SingleNumber_136.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class SingleNumber_136 {
+    public static int singleNumber(int[] nums) {
+
+        //Input: nums = [2,2,1]
+        //Output: 1
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums) {
+            if(set.contains(num)) {
+                set.remove(num);
+            }else {
+                set.add(num);
+            }
+        }
+        for (int i: set) return i;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 4, 1, 2, 1, 2};
+        var result = singleNumber(nums);
+        System.out.println(result);
+    }
+}
